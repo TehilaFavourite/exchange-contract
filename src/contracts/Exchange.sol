95abadc35766c1091c1c35be0c2d1f726f3c9105
tehilafavourite <tehilafavourite@gmail.com>
2022-07-16 02:33:48 +0100

    added order

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -9,12 +9,45 @@ contract Exchange {
 
         address public feeAccount;
         uint256 public feePercent;
+        uint256 public orderCount;
         address constant ETHER = address(0);
 
+    struct _Order {
+        uint256 id; 
+        address user; //person who made the order
+        address tokenGet; //token they want to purchase
+        uint256 amountGet; //amount of token user wants to get
+        address tokenGive; //token they will use in the trade i.e token they will give
+        uint256 amountGive; // the amount they will give
+        uint256 timestamp; // the actual time the order was created
+            }    
+
     mapping(address => mapping(address => uint256)) public trackDeposit;
+    mapping(uint256 => _Order) public orders;
+    mapping(uint256 => bool) public orderCancelled;
+    
 
     event Deposit(address indexed token, address indexed msgSender, uint256 amount, uint256 balance);
     event Withdraw(address indexed token, address indexed user, uint256 amount, uint256 balance);
+    event Order(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
+
+    event Cancel(
+        uint256 id,
+        address user,
+        address tokenGet,
+        uint256 amountGet,
+        address tokenGive,
+        uint256 amountGive,
+        uint256 timestamp
+    );
 
     constructor (address _feeAccount, uint256 _feePercent) {
         feeAccount = _feeAccount;
@@ -47,7 +80,25 @@ contract Exchange {
         require(_token != ETHER, "you cannot withdraw ether");
         require(trackDeposit[_token][msg.sender] >= _amount, "insufficient balance");
         trackDeposit[_token][msg.sender] = trackDeposit[_token][msg.sender].sub(_amount);
-        require(Token(_token).transfer(msg.sender, _amount), "the reason");
+        require(Token(_token).transfer(msg.sender, _amount), "denied");
         emit Withdraw(_token, msg.sender, _amount, trackDeposit[_token][msg.sender]);
+    }
+
+    function balanceOf(address _token, address _user) external view returns (uint256) {
+        return trackDeposit[_token][_user];
+    }
+
+    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) external {
+        orderCount = orderCount.add(1);
+        orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+        emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, block.timestamp);
+    }
+
+    function cancelOrder(uint256 _id) external {
+        _Order storage _order = orders[_id];
+        require(address(_order.user) == msg.sender, "not your order");
+        require(_order.id == _id, "order does not exist"); // The order must exist
+        orderCancelled[_id] = true;
+        emit Cancel(_order.id, msg.sender, _order.tokenGet, _order.amountGet, _order.tokenGive, _order.amountGive, block.timestamp);
     }
 }
--- test/Exchange.test.js
+++ test/Exchange.test.js
@@ -1,17 +1,20 @@
 const EVM_REVERT = "VM Exception while processing transaction: revert";
 const ETHER_ADDRESS = "0x0000000000000000000000000000000000000000";
 
+
 const Token = artifacts.require("Token");
 const Exchange = artifacts.require("Exchange");
 const Web3 = require("web3");
 
-const tokens = (n) => {
+const ether = (n) => {
   return new Web3.utils.BN(Web3.utils.toWei(n.toString(), "ether"));
 };
+const tokens = (n) => ether(n)
+
 require("chai").use(require("chai-as-promised")).should();
 
 contract("Exchange", function (accounts) {
-  [owner, feeAcct, user1] = accounts;
+  [owner, feeAcct, user1, user2] = accounts;
 
   let exchange;
   let tokenInstance;
@@ -39,33 +42,73 @@ contract("Exchange", function (accounts) {
   });
 
   describe("deposit and withdraw success", async () => {
-    it("tracks the amount", async () => {
-      await tokenInstance.approve(exchange.address, amount, { from: user1 });
-      deposit = await exchange.depositToken(
-        tokenInstance.address,
-        amount,
-        { from: user1 }
-      );
-      await tokenInstance.balanceOf(exchange.address);
-      console.log("the exchange balance is:", amount.toString());
-      const depBal = await exchange.trackDeposit(tokenInstance.address, user1);
-      console.log("the new deposit balance is: ", depBal.toString());
-    });
+        it("tracks the amount", async () => {
+            await tokenInstance.approve(exchange.address, amount, { from: user1 });
+            deposit = await exchange.depositToken(
+            tokenInstance.address,
+            amount,
+            { from: user1 }
+            );
+            await tokenInstance.balanceOf(exchange.address);
+            console.log("the exchange balance is:", amount.toString());
+            const depBal = await exchange.trackDeposit(tokenInstance.address, user1);
+            console.log("the new deposit balance is: ", depBal.toString());
+        });
 
-    it("tracks depositing ether", async () => {
-        await exchange.depositEther({ from: user1, value: amount});
-        const ethBal = await exchange.trackDeposit(ETHER_ADDRESS, user1);
-        console.log("the ether balance is: ", ethBal.toString()) 
-    })
+        it("tracks depositing ether", async () => {
+            await exchange.depositEther({ from: user1, value: amount});
+            const ethBal = await exchange.trackDeposit(ETHER_ADDRESS, user1);
+            console.log("the ether balance is: ", ethBal.toString()) 
+        })
 
-    it('reverts when Ether is sent', async () => {
-        await exchange.sendTransaction({ value: 1, from: user1 }).should.be.rejectedWith(EVM_REVERT)
-      }) 
+        it('reverts when Ether is sent', async () => {
+            await exchange.sendTransaction({ value: 1, from: user1 }).should.be.rejectedWith(EVM_REVERT)
+            })
+            
+        it('withdraws token funds', async () => {
+            const tokenbalance = await exchange.trackDeposit(tokenInstance.address, user1)
+            tokenbalance.toString().should.equal('0')
+            })  
+
+        it('withdraws Ether funds', async () => {
+            const etherbalance = await exchange.trackDeposit(ETHER_ADDRESS, user1)
+            etherbalance.toString().should.equal('0')
+            })
+
+        it('returns user balance', async () => {
+            exchange.depositEther({ from: user1, value: amount })
+            const result = await exchange.balanceOf(ETHER_ADDRESS, user1)
+            console.log("the user balance is after withdrawal: ", result.toString());;
+            })
+        
+        it("tracks the newly created order", async () => {
+            await exchange.makeOrder(tokenInstance.address, tokens(1), ETHER_ADDRESS, ether(1), { from: user1 })
+            await exchange.orderCount();
+            console.log("the new order created is: ", 1);
+            const order = await exchange.orders('1')
+            order.id.toString().should.equal('1', 'id is correct')
+            order.user.should.equal(user1, 'user is correct')
+            order.tokenGet.should.equal(tokenInstance.address, 'tokenGet is correct')
+            order.amountGet.toString().should.equal(tokens(1).toString(), 'amountGet is correct')
+            order.tokenGive.should.equal(ETHER_ADDRESS, 'tokenGive is correct')
+            order.amountGive.toString().should.equal(ether(1).toString(), 'amountGive is correct')
+            order.timestamp.toString().length.should.be.at.least(1, 'timestamp is present')
+        })
+
+        it("takes order actions", async () => {
+            // user1 deposits ether
+            await exchange.depositEther({ from: user1, value: ether(1) })
+            await exchange.makeOrder(tokenInstance.address, tokens(1), ETHER_ADDRESS, ether(1), { from: user1 })
+            
+        })
+
+        it("cancel orders", async () => {
+            await exchange.cancelOrder('1', { from: user1 })
+            const orderCancelled = await exchange.orderCancelled(1)
+            orderCancelled.should.equal(true)
+        })
     
-    it('withdraws Ether funds', async () => {
-        const balance = await exchange.trackDeposit(ETHER_ADDRESS, user1)
-        balance.toString().should.equal('0')
-      })
+      
       
   });
   
@@ -84,5 +127,15 @@ contract("Exchange", function (accounts) {
         .depositToken(tokenInstance.address, amount, { from: user1 })
         .should.be.rejectedWith(EVM_REVERT);
     });
+
+    it('rejects invalid order ids', async () => {
+        const invalidOrderId = 99999
+        await exchange.cancelOrder(invalidOrderId, { from: user1 }).should.be.rejectedWith(EVM_REVERT)
+      })
+
+      it('rejects unauthorized cancelations', async () => {
+        // Try to cancel the order from another user
+        await exchange.cancelOrder('1', { from: user2 }).should.be.rejectedWith(EVM_REVERT)
+      })
   });
 });
